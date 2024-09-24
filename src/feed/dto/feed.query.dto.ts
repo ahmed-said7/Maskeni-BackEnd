932e8c08fc582a8bb2aa06ef33445042a36859c0
ahmed-said7 <engahmedsaid125@gmail.com>
2024-09-24 20:39:15 +0300

    locations

--- src/feed/dto/feed.query.dto.ts
+++ src/feed/dto/feed.query.dto.ts
@@ -1,13 +1,25 @@
-import { IsNumber, IsOptional } from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsMongoId, IsNumber, IsOptional } from 'class-validator';
 
 export class FeedQueryDto {
   @IsNumber()
+  @ApiPropertyOptional()
   @IsOptional()
   page: number;
   @IsOptional()
+  @ApiPropertyOptional()
   @IsNumber()
   limit: number;
-  quarter: string;
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsMongoId()
   country: string;
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsMongoId()
   city: string;
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsMongoId()
+  quarter: string;
 }
