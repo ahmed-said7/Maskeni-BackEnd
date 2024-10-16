dbcd2f37b797a5f131dab7d3a806ac84ff2a4d23
ahmed-said7 <engahmedsaid125@gmail.com>
2024-10-16 17:55:47 +0300

    paypal module

--- src/feed/feed.controller.ts
+++ src/feed/feed.controller.ts
@@ -1,8 +1,11 @@
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get, Query, Req, UseGuards } from '@nestjs/common';
 import { FeedService } from './feed.service';
 import { FeedQueryDto } from './dto/feed.query.dto';
 import { AuthenticationGuard } from 'src/common/guards/authentication.guard';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { AuthorizationGuard } from 'src/common/guards/authorization.guard';
+import { All_Role } from 'src/common/enum';
+import { Roles } from 'src/common/decorator/roles';
 
 @ApiTags('Feed') // Tag for grouping related endpoints
 @Controller('feed')
@@ -10,7 +13,8 @@ export class FeedController {
   constructor(private readonly feedService: FeedService) {}
 
   @Get()
-  @UseGuards(AuthenticationGuard)
+  @UseGuards(AuthenticationGuard, AuthorizationGuard)
+  @Roles(All_Role.User)
   @ApiOperation({
     summary: 'Get Feed',
     description: 'Retrieves the feed items based on provided query parameters.',
@@ -27,7 +31,7 @@ export class FeedController {
     status: 400,
     description: 'Bad request, invalid query parameters.',
   })
-  async getFeed(@Query() query: FeedQueryDto) {
-    return this.feedService.getFeed(query);
+  async getFeed(@Query() query: FeedQueryDto, @Req() req: any) {
+    return this.feedService.getFeed(query, req.userId);
   }
 }
